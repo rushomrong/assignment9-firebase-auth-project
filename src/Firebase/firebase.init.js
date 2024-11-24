bc2bc8855c6b9eaf5bbbc3a3a978049a996857a9
rushomrong <rushomarak@gmail.com>
2024-11-24 16:27:35 +0600

    key update

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -1,17 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { firebaseKeys } from "../components/Account/PrivateRoute/Auth/firebaseKeys ";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCLlVkuqPNZuNKcQQp7DdNHD_csIPwlEY8",
-  authDomain: "react-wildlife-safaris-rusho.firebaseapp.com",
-  projectId: "react-wildlife-safaris-rusho",
-  storageBucket: "react-wildlife-safaris-rusho.firebasestorage.app",
-  messagingSenderId: "344294983171",
-  appId: "1:344294983171:web:352ef1a28dc5123f4d7036",
+  ...firebaseKeys,
 };
 
 // Initialize Firebase
---
+++ src/components/Account/PrivateRoute/Auth/firebaseKeys .js
@@ -0,0 +1,9 @@
+// firebaseKeys.js
+export const firebaseKeys = {
+  apiKey: "AIzaSyCLlVkuqPNZuNKcQQp7DdNHD_csIPwlEY8",
+  authDomain: "react-wildlife-safaris-rusho.firebaseapp.com",
+  projectId: "react-wildlife-safaris-rusho",
+  storageBucket: "react-wildlife-safaris-rusho.firebasestorage.app",
+  messagingSenderId: "344294983171",
+  appId: "1:344294983171:web:352ef1a28dc5123f4d7036",
+};
